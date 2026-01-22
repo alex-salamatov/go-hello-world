80f904e520cfa7b213d1c3d278642b3198e47ccf
alex-salamatov <alex.salamatov@gmail.com>
2026-01-22 16:19:09 +0100

    Initial Hello World program

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alex-salamatov/go-hello-world
+
+go 1.25.6
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, SAS!")
+	fmt.Println("Hello, Go + GitHub!")
+}
