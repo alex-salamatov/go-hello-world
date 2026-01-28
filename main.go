37adc301ad9ef9cb880679f505e7069565ae14e0
alex-salamatov <alex.salamatov@gmail.com>
2026-01-28 10:09:59 +0100

    Updated implementation by addressing feedback from Alexei

--- main.go
+++ main.go
@@ -10,11 +10,7 @@ import (
 
 const maxAge = 100 //assuming older people cannot work with computers :-)
 
-func isAgeOk(age int, err error) bool {
-	if err != nil {
-		return false
-	}
-
+func isAgeOk(age int) bool {
 	if (age < 0) || (age > maxAge) {
 		return false
 	}
@@ -40,14 +36,16 @@ func main() {
 		fmt.Println("Failed to read age")
 		return
 	}
-	age, ageError := strconv.Atoi(scanner.Text())
-
-	fmt.Printf("Hello %s, your year of birth is ", name)
-	if isAgeOk(age, ageError) {
-		birthYear = birthYear - age
-		fmt.Printf("%d or %d\n", birthYear, birthYear+1) //you never know actual year of birth without knowing exact birthday
-	} else {
-		fmt.Println("unknown.")
+	age, err := strconv.Atoi(scanner.Text())
+	birthYearString := "unknown"
+	if err != nil {
+		fmt.Println("Entered age is not valid")
 	}
 
+	fmt.Printf("Hello %s, your year of birth is ", name)
+	if isAgeOk(age) {
+		birthYear = birthYear - age
+		birthYearString = fmt.Sprintf("%d or %d\n", birthYear, birthYear+1) //you never know actual year of birth without knowing exact birthday
+	}
+	fmt.Println(birthYearString)
 }
