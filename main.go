40236b339c9f83556051db48214d0a5ab36f9f75
alex-salamatov <alex.salamatov@gmail.com>
2026-01-28 10:51:01 +0100

    Added forced age input until it is valid

--- main.go
+++ main.go
@@ -32,20 +32,28 @@ func main() {
 	var age int
 	var birthYear int = time.Now().Year()
 
-	if !scanner.Scan() {
-		fmt.Println("Failed to read age")
-		return
-	}
-	age, err := strconv.Atoi(scanner.Text())
-	birthYearString := "unknown"
-	if err != nil {
-		fmt.Println("Entered age is not valid")
+	for {
+		if !scanner.Scan() {
+			fmt.Println("Failed to read age")
+			return
+		}
+
+		var err error
+		age, err = strconv.Atoi(scanner.Text())
+
+		if err != nil {
+			fmt.Println("Entered age is not a number, please try again to enter integer value")
+			continue
+		}
+
+		if !isAgeOk(age) {
+			fmt.Println("Entered age is not in valid range, please try again and enter en integer value in a range [0 - 100]")
+			continue
+		}
+
+		break
 	}
 
-	fmt.Printf("Hello %s, your year of birth is ", name)
-	if isAgeOk(age) {
-		birthYear = birthYear - age
-		birthYearString = fmt.Sprintf("%d or %d\n", birthYear, birthYear+1) //you never know actual year of birth without knowing exact birthday
-	}
-	fmt.Println(birthYearString)
+	birthYear = birthYear - age
+	fmt.Printf("Hello %s, your year of birth is %d or %d\n", name, birthYear, birthYear+1) //you never know actual year of birth without knowing exact birthday
 }
